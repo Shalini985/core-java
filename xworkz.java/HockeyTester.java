2a5b3c9eac98938daac6426b12acd6c486709bec
shalini985 <shalini.xworkz@gmail.com>
2022-09-10 08:48:21 +0530

    java files

---
+++ xworkz.java/HockeyTester.java
@@ -0,0 +1,9 @@
+class HockeyTester{
+ public static void main(String a[]){
+ 
+  String[] teamMembers={"Hardik singh","PR Sreejesh","amit Rohidas","gurjant singh","birendra lakra","sumit","varun kumar","akashdeep singh","vivek sagar prasad","lalithkumar upadya","krishan bahadur pathak","shamsher singh","manpreet singh","harmanpreet singh","nilakanta sharma","simiranjeet singh"  };
+  Hockey.getTeamMembers(teamMembers);
+ }
+
+
+}
