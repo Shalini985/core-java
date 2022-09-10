f1ca5dbb674d8b826ce15d4c79b7858e4ac73e0d
shalini985 <shalini.xworkz@gmail.com>
2022-09-10 09:12:39 +0530

    commting array related fiels

---
+++ RestuarantTester.java
@@ -0,0 +1,15 @@
+class RestuarantTester{
+ public static void main(String a[]){
+	 
+	 String name = "vivantha taj";
+
+ Restuarant.getName(name);
+	 
+ String restuarant[]={"chitharanna","pizza","rice","brager","mosaranna"};
+    
+   
+   Restuarant.getFoodMenu(restuarant);
+   
+
+ }
+}
